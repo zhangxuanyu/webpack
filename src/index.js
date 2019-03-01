f5f5c8464607d8402abd379286ee844628a15984
Jun Zhang <jzhang@baina.com>
2019-03-01 16:37:29 +0800

    webpack 热更新

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import a from './data.js'
 import './css/a.css'
 
-console.log(a+11)
+console.log(10)
 
 // 还需要在主要的js文件里写入下面这段代码
 if (module.hot) {
--- webpack.config.js
+++ webpack.config.js
@@ -8,31 +8,31 @@ let ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: {
         index:'./src/index.js',
-        out:'./src/out.js'
+        // out:'./src/out.js'
     },    // 入口文件
     output: {
         filename: '[name].js',      // 打包后的文件名称
         path: path.resolve('dist')  // 打包后的目录，必须是绝对路径
     },
     // 提取公共代码
-   optimization: {
-    splitChunks: {
-        cacheGroups: {
-            vendor: {   // 抽离第三方插件
-                test: /node_modules/,   // 指定是node_modules下的第三方包
-                chunks: 'initial',
-                name: 'vendor',  // 打包后的文件名，任意命名    
-                // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
-                priority: 10    
-            },
-            utils: { // 抽离自己写的公共代码，utils这个名字可以随意起
-                chunks: 'initial',
-                name: 'utils',  // 任意命名
-                minSize: 0    // 只要超出0字节就生成一个新包
-            }
-        }
-    }
-   },
+//    optimization: {
+//     splitChunks: {
+//         cacheGroups: {
+//             vendor: {   // 抽离第三方插件
+//                 test: /node_modules/,   // 指定是node_modules下的第三方包
+//                 chunks: 'initial',
+//                 name: 'vendor',  // 打包后的文件名，任意命名    
+//                 // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
+//                 priority: 10    
+//             },
+//             utils: { // 抽离自己写的公共代码，utils这个名字可以随意起
+//                 chunks: 'initial',
+//                 name: 'utils',  // 任意命名
+//                 minSize: 0    // 只要超出0字节就生成一个新包
+//             }
+//         }
+//     }
+//    },
     devServer: {
         contentBase: './dist',
         host: 'localhost',      // 默认是localhost
